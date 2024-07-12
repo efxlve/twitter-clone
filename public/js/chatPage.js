e4c4be42658603c7fe87bf58f63c5fcb7cfa03ca
Muharrem Efe Çayırbahçe <muharremefecayirbahce@gmail.com>
2024-07-12 23:45:52 +0300

    🚀Project added

---
+++ public/js/chatPage.js
@@ -0,0 +1,47 @@
+$(document).ready(() => {
+    $.get(`/api/chats/${chatId}`, (data) => $("#chatName").text(getChatName(data)))
+});
+
+$("#chatNameButton").click(() => {
+    var name = $("#chatNameTextbox").val().trim();
+    
+    $.ajax({
+        url: "/api/chats/" + chatId,
+        type: "PUT",
+        data: { chatName: name },
+        success: ( data, status, xhr ) => {
+            if(xhr.status != 204) {
+                alert("Could not update.");
+            }
+            else {
+                location.reload();
+            }
+        }
+    })
+});
+
+$(".sendMessageButton").click(() => {
+    messageSubmitted();
+});
+
+$(".inputTextbox").keydown((event) => {
+    if(event.which === 13 && !event.shiftKey) {
+        messageSubmitted();
+        return false;
+    }
+});
+
+function messageSubmitted() {
+    var content = $(".inputTextbox").val().trim();
+
+    if(content != "") {
+        sendMessage(content);
+        $(".inputTextbox").val("");
+    }
+};
+
+function sendMessage(content) {
+    $.post("/api/messages", { content: content, chatId }, (data, status, xhr) => {
+        console.log(data);
+    })
+}
