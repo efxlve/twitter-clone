d0abc2618bece78b88a28c5defb9a1f82ee927a0
Muharrem Efe Çayırbahçe <muharremefecayirbahce@gmail.com>
2024-07-15 00:36:25 +0300

    🚀Project added

---
+++ public/js/notificationsPage.js
@@ -0,0 +1,56 @@
+$(document).ready(() => {
+    $.get("/api/notifications", (data) => {
+        outputNotificationList(data, $(".resultsContainer"));
+    })
+});
+
+function outputNotificationList(notifications, container) {
+    notifications.forEach(notification => {
+        var html = createNotificationHtml(notification);
+        container.append(html);
+    });
+
+    if (notifications.length == 0) {
+        container.append("<span class='noResults'>Nothing to show.</span>");
+    };
+};
+
+function createNotificationHtml(notification) {
+    var userFrom = notification.userFrom;
+    var text = getNotificationText(notification);
+
+    return `<a href='#' class='resultListItem'=>
+                <div class='resultsImageContainer'>
+                    <img src='${userFrom.profilePicture}'>
+                </div>
+                <div class='resultsDetailsContainer ellipsis'>
+                    <span class='ellipsis'>${text}</span>
+                </div>
+            </a>`;
+};
+
+function getNotificationText(notification) {
+    var userFrom = notification.userFrom;
+
+    if(!userFrom) {
+        return alert("User from data not populated.")
+    };
+
+    var userFromName = `${userFrom.firstName} ${userFrom.lastName}`;
+    var text;
+
+    if(notification.notificationType == "retweet") {
+        text = `${userFromName} retweeted one of your tweets`;
+    }
+    else if(notification.notificationType == "postLike") {
+        text = `${userFromName} liked one of your tweets`;
+    }
+    else if(notification.notificationType == "reply") {
+        text = `${userFromName} replied to one of your tweets`;
+    }
+    else if (notification.notificationType == "follow") {
+        text = `${userFromName} followed you`;
+    }
+
+    return `<span class='ellipsis'>${text}</span>`;
+};
