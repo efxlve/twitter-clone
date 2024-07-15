837b190609d02a7898a9bf53dd465d7354d395c5
Muharrem Efe Çayırbahçe <muharremefecayirbahce@gmail.com>
2024-07-15 16:32:11 +0300

    🚀Project added

--- public/js/common.js
+++ public/js/common.js
@@ -3,6 +3,11 @@ var cropper;
 var timer;
 var selectedUsers = [];
 
+$(document).ready(() => {
+    refreshMessagesBadge();
+    refreshNotificationsBadge();
+})
+
 $("#postTextarea, #replyTextarea").keyup(event => {
     var textbox = $(event.target);
     var value = textbox.val().trim();
@@ -631,6 +636,7 @@ function messageReceived(newMessage) {
     else {
         addChatMessageHtml(newMessage);
     }
+    refreshMessagesBadge(newMessage);
 }
 
 function markNotificationsAsOpened(notificationId = null, callback = null) {
@@ -642,5 +648,31 @@ function markNotificationsAsOpened(notificationId = null, callback = null) {
         url: url,
         type: "PUT",
         success: () => callback()
+    })
+}
+
+function refreshMessagesBadge() {
+    $.get("/api/chats", { unreadOnly: true }, (data) => {
+        var numResults = data.length;
+
+        if(numResults > 0) {
+            $("#messagesBadge").text(numResults).addClass("active");
+        }
+        else {
+            $("#messagesBadge").text("").removeClass("active");
+        }
+    })
+}
+
+function refreshNotificationsBadge() {
+    $.get("/api/notifications", { unreadOnly: true }, (data) => {
+        var numResults = data.length;
+
+        if(numResults > 0) {
+            $("#notificationBadge").text(numResults).addClass("active");
+        }
+        else {
+            $("#notificationBadge").text("").removeClass("active");
+        }
     })
 }
